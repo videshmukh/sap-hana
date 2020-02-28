530a0b32654263abad1a243de103a7b9043e4d72
Vinod Deshmukh <51819095+videshmukh@users.noreply.github.com>
2020-02-28 14:25:14 +0100

    Update set-sp.sh

--- deploy/vm/modules/single_node_hana/set-sp.sh
+++ deploy/vm/modules/single_node_hana/set-sp.sh
@@ -1,11 +1,11 @@
 # configure service principal for Ansible
-export AZURE_SUBSCRIPTION_ID='7e41ed69-38e5-4b72-98ef-85c6830275c7'
-export AZURE_CLIENT_ID='5740ed7b-3224-40c6-9662-a061d1a574df'
-export AZURE_SECRET='8b7e5e78-2129-4f79-88c7-c39aad3bc1ce'
-export AZURE_TENANT='72f988bf-86f1-41af-91ab-2d7cd011db47'
+export AZURE_SUBSCRIPTION_ID=''
+export AZURE_CLIENT_ID=''
+export AZURE_SECRET=''
+export AZURE_TENANT=''
 
 # configure service principal for Terraform
-export ARM_SUBSCRIPTION_ID='7e41ed69-38e5-4b72-98ef-85c6830275c7'
-export ARM_CLIENT_ID='5740ed7b-3224-40c6-9662-a061d1a574df'
-export ARM_CLIENT_SECRET='8b7e5e78-2129-4f79-88c7-c39aad3bc1ce'
-export ARM_TENANT_ID='72f988bf-86f1-41af-91ab-2d7cd011db47'
+export ARM_SUBSCRIPTION_ID=''
+export ARM_CLIENT_ID=''
+export ARM_CLIENT_SECRET=''
+export ARM_TENANT_ID=''
